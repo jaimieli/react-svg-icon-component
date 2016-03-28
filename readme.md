67c6c6d16e0076d5bd6ae822720c6e1f0971273a
jaimieli <jaimieli@gmail.com>
2016-03-28 10:17:11 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# react-svg-icon-component
+svg icons as react components
+
+##installation:
+```sh
+npm install --save react-svg-icon-component
+```
+
+##usage:
+```js
+const reactSvgIconComponent = require('react-svg-icon-component');
+```
---
+++ test/specs.js
@@ -0,0 +1,7 @@
+"use strict";
+var reactSvgIconComponent = require('../index');
+describe("react-svg-icon-component", function () {
+    it("should be svg icons as react components", function () {
+        // test your code here
+    });
+});
